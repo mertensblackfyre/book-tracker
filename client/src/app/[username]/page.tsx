b2f5d741ee1793f66af902e3f0d430db726c39a4
amr8644 <amr8644@gmail.com>
2023-08-20 01:28:41 +0300

    Added Book Card

--- client/src/app/[username]/page.tsx
+++ client/src/app/[username]/page.tsx
@@ -1,26 +1,26 @@
 import Container from "@/components/layouts/Container";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { FC } from "react";
 import BookList from "./components/BookList";
 
 type UserProfilePageType = {
-    params: { username: string }
-}
+   params: { username: string };
+};
 
 const UserProfilePage: FC<UserProfilePageType> = ({ params }) => {
-
-    return (
-        <Container>
+   return (
+      <>
+         <Container>
             <header className="mb-[25px]">
-                <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                    {params.username}'s books
-                </h2>
-            </header> 
+               <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                  {params.username}'s books
+               </h2>
+            </header>
             <div>
-                <BookList /> 
+               <BookList />
             </div>
-        </Container>
-    );
-}
+         </Container>
+      </>
+   );
+};
 
 export default UserProfilePage;
